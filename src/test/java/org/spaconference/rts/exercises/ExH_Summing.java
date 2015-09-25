00de4d569a28ff2d94c6abcc5344b5d95f245337
Peter Kofler <peter.kofler@code-cop.org>
2015-09-25 20:27:04 +0200

    ExH

--- src/test/java/org/spaconference/rts/exercises/ExH_Summing.java
+++ src/test/java/org/spaconference/rts/exercises/ExH_Summing.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.spaconference.rts.runner.ExampleRunner;
 
 import java.util.function.ToIntFunction;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,6 +22,32 @@ public class ExH_Summing {
             result += i;
         }
         return result;
+    }
+
+    @Way
+    public static int step1_intStream(int[] ints) {
+        int result = 0;
+        for (int i : (Iterable<Integer>) IntStream.of(ints)::iterator) {
+            result += i;
+        }
+        return result;
+    }
+
+    @Way
+    public static int step2_forEach(int[] ints) {
+        final int[] result = {0};
+        IntStream.of(ints).forEach(i -> result[0] += i);
+        return result[0];
+    }
+
+    @Way
+    public static int step3_reduce(int[] ints) {
+        return IntStream.of(ints).reduce(0, (a, b) -> a + b);
+    }
+
+    @Way
+    public static int step4_sum(int[] ints) {
+        return IntStream.of(ints).sum();
     }
 
     @Test
