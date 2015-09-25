9810d28ddfc4f64df716ee2076c7627f18f93dc4
Peter Kofler <peter.kofler@code-cop.org>
2015-09-25 19:49:20 +0200

    ExC

--- src/test/java/org/spaconference/rts/exercises/ExC_Collecting.java
+++ src/test/java/org/spaconference/rts/exercises/ExC_Collecting.java
@@ -7,6 +7,9 @@ import org.spaconference.rts.runner.ExampleRunner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -24,6 +27,23 @@ public class ExC_Collecting {
             result.add(thing);
         }
         return result;
+    }
+
+    @Way
+    public static List<String> step1_iterate(Iterable<String> things) {
+        List<String> result = new ArrayList<>();
+        StreamSupport.stream(things.spliterator(), false).forEach(result::add);
+        return result;
+    }
+
+    @Way
+    public static List<String> step2_collect(Iterable<String> things) {
+        return StreamSupport.stream(things.spliterator(), false).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    @Way
+    public static List<String> step2_collect_toList(Iterable<String> things) {
+        return StreamSupport.stream(things.spliterator(), false).collect(Collectors.toList());
     }
 
     @Test
