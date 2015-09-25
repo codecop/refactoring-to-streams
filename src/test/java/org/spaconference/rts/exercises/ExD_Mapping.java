02abaae8a06abc0d4434e5cb0e55db06c6fe6d85
Peter Kofler <peter.kofler@code-cop.org>
2015-09-25 19:55:26 +0200

    ExD

--- src/test/java/org/spaconference/rts/exercises/ExD_Mapping.java
+++ src/test/java/org/spaconference/rts/exercises/ExD_Mapping.java
@@ -7,6 +7,9 @@ import org.spaconference.rts.runner.ExampleRunner;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -24,6 +27,36 @@ public class ExD_Mapping {
             result.add(Integer.parseInt(string));
         }
         return result;
+    }
+
+    @Way
+    public static List<Integer> step1_iterate_stream(List<String> strings) {
+        Stream<String> stream = StreamSupport.stream(strings.spliterator(), false);
+        Iterable<String> iterable = stream::iterator;
+        List<Integer> result = new ArrayList<>();
+        for (String string : iterable) {
+            result.add(Integer.parseInt(string));
+        }
+        return result;
+    }
+
+    @Way
+    public static List<Integer> step2_map_stream(List<String> strings) {
+        Stream<Integer> stream = StreamSupport.stream(strings.spliterator(), false).
+                                               map(string -> Integer.parseInt(string));
+        Iterable<Integer> iterable = stream::iterator;
+        List<Integer> result = new ArrayList<>();
+        for (Integer i : iterable) {
+            result.add(i);
+        }
+        return result;
+    }
+
+    @Way
+    public static List<Integer> step3_map_collect(List<String> strings) {
+        return StreamSupport.stream(strings.spliterator(), false).
+                map(Integer::parseInt).
+                collect(Collectors.toList());
     }
 
     @Test
